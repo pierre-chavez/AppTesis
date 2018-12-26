ca820528369a7ed5aac32a78719afaafdd58f208
pierre <pierre_chavez@outlook.com>
2018-12-26 11:37:11 -0500

    validacion de modulo de registro de paciente

---
+++ app/src/main/java/com/example/uees2/myapplication/Paciente.java
@@ -0,0 +1,93 @@
+package com.example.uees2.myapplication;
+
+import java.util.Date;
+
+public class Paciente {
+
+    String cedula;
+    String nombres;
+    String apellidos;
+    String fechaRegistro;
+    String genero;
+    int habitcion;
+    String nombreContacto;
+    String numeroContacto;
+
+    public Paciente() {
+    }
+
+    public Paciente(String cedula, String nombres, String apellidos, String fechaRegistro, String genero, int habitcion, String nombreContacto, String numeroContacto) {
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fechaRegistro = fechaRegistro;
+        this.genero = genero;
+        this.habitcion = habitcion;
+        this.nombreContacto = nombreContacto;
+        this.numeroContacto = numeroContacto;
+    }
+
+    public int getHabitcion() {
+        return habitcion;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getNombreContacto() {
+        return nombreContacto;
+    }
+
+    public String getNumeroContacto() {
+        return numeroContacto;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setHabitcion(int habitcion) {
+        this.habitcion = habitcion;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setFechaRegistro(String fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setNombreContacto(String nombreContacto) {
+        this.nombreContacto = nombreContacto;
+    }
+
+    public void setNumeroContacto(String numeroContacto) {
+        this.numeroContacto = numeroContacto;
+    }
+}
---
+++ app/src/main/java/com/example/uees2/myapplication/Registro.java
@@ -0,0 +1,117 @@
+package com.example.uees2.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Registro extends AppCompatActivity {
+
+    Date fechaRegistro;
+    EditText editTextCedula, editTextNombres, editTextApellidos, editTextNombreContato, editTextCelularContacto;
+    Spinner spinnerGenero,spinnerHabitacion;
+    Button buttonRegistrar;
+
+    DatabaseReference databasePacientes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+
+        databasePacientes = FirebaseDatabase.getInstance().getReference("Persona");
+
+        editTextCedula = findViewById(R.id.editTextCedula);
+        editTextNombres = findViewById(R.id.editTextNombre);
+        editTextApellidos = findViewById(R.id.editTextApellidos);
+        editTextNombreContato = findViewById(R.id.editTextNombreContacto);
+        editTextCelularContacto = findViewById(R.id.editTextCelularContacto);
+        spinnerGenero = findViewById(R.id.spinnerGenero);
+        spinnerHabitacion = findViewById(R.id.spinnerHabitacion);
+        buttonRegistrar = findViewById(R.id.buttonRegistrar);
+
+
+        buttonRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RegistrarPaciente();
+            }
+        });
+
+
+    }
+
+
+    private void RegistrarPaciente(){
+
+        String cedula = editTextCedula.getText().toString().trim();
+        String nombres = editTextNombres.getText().toString().trim();
+        String apellidos = editTextApellidos.getText().toString().trim();
+        String nombreContacto = editTextNombreContato.getText().toString();
+        String celularContacto = editTextCelularContacto.getText().toString();
+        String genero = spinnerGenero.getSelectedItem().toString();
+        String habitacion = spinnerHabitacion.getSelectedItem().toString();
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("dd/mm/yyyy");
+        String strDate = mdformat.format(calendar.getTime());
+
+        if (cedula.isEmpty()){
+            editTextCedula.setError("Debe ingresar cedula de paciente");
+            editTextCedula.requestFocus();
+            return;
+        }
+
+        if (nombres.isEmpty()){
+            editTextNombres.setError("Debe ingresar los nombres");
+            editTextNombres.requestFocus();
+            return;
+        }
+
+        if (apellidos.isEmpty()){
+            editTextApellidos.setError("Debe ingresar los apellidos");
+            editTextApellidos.requestFocus();
+            return;
+        }
+
+        if (nombreContacto.isEmpty()){
+            editTextNombreContato.setError("Debe ingresar nombre de persona de contacto");
+            editTextNombreContato.requestFocus();
+            return;
+        }
+
+        if (celularContacto.isEmpty()){
+            editTextCelularContacto.setError("Debe ingresar celular de persona de contacto");
+            editTextCelularContacto.requestFocus();
+            return;
+        }
+
+
+
+        String id = cedula;
+        int nHabitacion = Integer.parseInt(habitacion);
+
+        Paciente paciente = new Paciente(cedula,nombres,apellidos,strDate,genero,nHabitacion,nombreContacto,celularContacto);
+
+        databasePacientes.child(id).setValue(paciente);
+
+        Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+}
