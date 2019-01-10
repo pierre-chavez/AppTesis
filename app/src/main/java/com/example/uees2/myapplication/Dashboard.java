b655976376662de95de9ee77c3db9a10f48d1d5d
kevinlupera <kevinlupera1997@outlook.com>
2019-01-09 23:36:20 -0500

    Intro primera vez

--- app/src/main/java/com/example/uees2/myapplication/Dashboard.java
+++ app/src/main/java/com/example/uees2/myapplication/Dashboard.java
@@ -1,17 +1,22 @@
 package com.example.uees2.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.onesignal.OneSignal;
+
 
 public class Dashboard extends AppCompatActivity {
 
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
+    public static final String TAG = "Version";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +35,36 @@ public class Dashboard extends AppCompatActivity {
         fragmentTransaction.replace(R.id.container, dashboardFrag);
         fragmentTransaction.commit();
 
-        Intent intent = new Intent(this, Informacion.class);
-        startActivity(intent);
+
+        switch (getFirstTimeRun(this)) {
+            case 0:
+                Log.d(TAG, "Es la primera vez!");
+                // acá haces el intent a tu activity especial
+                Intent intent = new Intent(this, Informacion.class);
+                startActivity(intent);
+                break;
+            case 1:
+                Log.d(TAG, "Ya has iniciado la app alguna vez");
+                //
+                break;
+            case 2:
+                Log.d(TAG, "Es una versión nueva");
+                //
+                break;
+        }
+
+
+    }
+
+    //Retorna: 0 primera vez / 1 no es primera vez / 2 nueva versión
+    public static int getFirstTimeRun(Context contexto) {
+        SharedPreferences sp = contexto.getSharedPreferences("MYAPP", 0);
+        int result, currentVersionCode = BuildConfig.VERSION_CODE;
+        int lastVersionCode = sp.getInt("FIRSTTIMERUN", -1);
+        if (lastVersionCode == -1) result = 0;
+        else
+            result = (lastVersionCode == currentVersionCode) ? 1 : 2;
+        sp.edit().putInt("FIRSTTIMERUN", currentVersionCode).apply();
+        return result;
     }
 }
--- app/src/main/java/com/example/uees2/myapplication/Informacion.java
+++ app/src/main/java/com/example/uees2/myapplication/Informacion.java
@@ -26,11 +26,10 @@ public class Informacion extends AppIntro {
                 R.drawable.monitor, ContextCompat.getColor(getApplicationContext(), R.color.Intro4)));
 
         addSlide(AppIntroFragment.newInstance("Instrucciones para configurar pulsera:",
-                "1. Encender la pulsera." +
-                        "2. Conectarse a la red WiFi 'pulsera' contraseña '12345678'." +
-                        "3. En la aplicación elegir la opción conectar." +
-                        "4. Ingresar las credenciales de la red WiFi con acceso a Internet." +
-                        "5. En la aplicación elegir la opción y enlazar la pulsera con un paciente registrado previamente.",
+                "1. Encender la pulsera y conectarse a la red WiFi 'pulsera' contraseña '12345678'.\n" +
+                        "2. En la aplicación elegir la opción conectar.\n" +
+                        "3. Ingresar las credenciales de la red WiFi con acceso a Internet.\n" +
+                        "4. En la aplicación elegir la opción enlazar para relacionar la pulsera con un paciente registrado previamente.",
                 R.drawable.instrucciones, ContextCompat.getColor(getApplicationContext(), R.color.Intro4)));
     }
 
