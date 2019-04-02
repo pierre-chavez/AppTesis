208332a971a4c0708017ccf69098aa15f3d0d856
pierre <pierre_chavez@outlook.com>
2019-04-02 10:01:56 -0500

    desenlazar paciente

--- app/src/main/java/com/example/uees2/myapplication/UserActivity.java
+++ app/src/main/java/com/example/uees2/myapplication/UserActivity.java
@@ -1,23 +1,38 @@
 package com.example.uees2.myapplication;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserActivity extends AppCompatActivity {
 
     DatabaseReference databaseUsuario;
+    DatabaseReference databaseNotificacion;
+
     EditText editTextEmail;
     Spinner spinnerRoles;
     Button buttonGuardar;
     Usuario usuario;
+    List<Usuario> listaTrabajadores;
+    public String listaAdmin = "";
+    public String listaEnfermero = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,7 @@ public class UserActivity extends AppCompatActivity {
 
         usuario = (Usuario) getIntent().getSerializableExtra(ListarUsuariosActivity.USUARIO);
         databaseUsuario = FirebaseDatabase.getInstance().getReference("Usuario");
+        databaseNotificacion = FirebaseDatabase.getInstance().getReference("Notificacion");
 
         editTextEmail = findViewById(R.id.editTextEmail);
         spinnerRoles = findViewById(R.id.spinnerRoles);
@@ -36,7 +52,7 @@ public class UserActivity extends AppCompatActivity {
         spinnerRoles.setSelection(getIndex(spinnerRoles, usuario.getRol()));
 
 
-
+        listaTrabajadores = new ArrayList<>();
 
         buttonGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,9 +85,78 @@ public class UserActivity extends AppCompatActivity {
         usuario.setRol(rol);
         databaseUsuario.child(usuario.getUserId()).setValue(usuario);
 
+        listaAdmin = getNotificacion();
+
+        Log.d("notificacion",""+listaAdmin.length());
+        Log.d("notificacion",listaAdmin);
+        Log.d("notificacion",""+listaEnfermero.length());
+        Log.d("notificacion",listaEnfermero);
+
+
+
         finish();
         Toast.makeText(this, "Usuario Actualizado", Toast.LENGTH_LONG).show();
 
         return true;
+    }
+
+
+    private boolean setNotificacion(String listaA ,String listaE){
+
+
+        databaseNotificacion.child("Admin").setValue(listaA);
+        databaseNotificacion.child("Enfermero").setValue(listaE);
+
+        return true;
+    }
+
+
+    protected String getNotificacion() {
+
+        databaseUsuario.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaTrabajadores.clear();
+                for(DataSnapshot pacienteSnapshot : dataSnapshot.getChildren()){
+                    Usuario usuario = pacienteSnapshot.getValue(Usuario.class);
+
+                    //listaTrabajadores.add(usuario);
+                    Log.d("notificacion",usuario.getRol());
+
+                    if(usuario.getRol().equals("Admin")){
+                        listaAdmin = listaAdmin+"\""+usuario.getPlayerId()+"\""+',';
+                    }
+
+                    if (usuario.getRol().equals("Enfermero")){
+                        listaEnfermero = listaEnfermero+"\""+usuario.getPlayerId()+"\""+',';
+                    }
+
+
+
+                }
+
+                //Toast.makeText(getApplicationContext(), listaAdmin , Toast.LENGTH_LONG).show();
+
+                if(listaAdmin.length()>0){
+                listaAdmin=listaAdmin.substring(0,listaAdmin.length()-1);
+                }
+                if(listaAdmin.length()>0) {
+                    listaEnfermero = listaEnfermero.substring(0, listaEnfermero.length() - 1);
+                }
+                Log.d("notificacion",listaAdmin);
+                setNotificacion(listaAdmin,listaEnfermero);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        return listaAdmin;
     }
 }
